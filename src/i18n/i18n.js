ce30171784f61a2ac261d9178ae1731869739b4e
Palacio2 <zaviyskiyxd@gmail.com>
2026-04-13 22:47:48 +0200

    fix: оновлення перекладів та структури i18n

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { api } from '../services/api';
+import { api } from '../services/api.js';
 
 i18n
   .use(LanguageDetector)
